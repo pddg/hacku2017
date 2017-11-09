def5475b9453c9e18d02a528c1d7f75d79eef45f
ppdg <pudding@mail.poyo.info>
2017-11-10 01:41:55 +0900

    もうひとりifttt通知送れるように変更

--- src/iot_stick/tracking/tasks.py
+++ src/iot_stick/tracking/tasks.py
@@ -3,33 +3,36 @@ import requests
 
 from django.conf import settings
 
-base_url = 'https://maker.ifttt.com/trigger/{event}/with/key/' + settings.IFTTT_KEY
+base_url = 'https://maker.ifttt.com/trigger/{event}/with/key/{IFTTT_KEY}'
 
 
 @task
 def send_outgoing_webhook(module_id, state, home, radius, out):
     from .models import NotificationLog, Module
     m = Module.objects.get(module_id=module_id)
-    log = NotificationLog()
-    http_choice = log.HttpStatusChoice
-    log.status = http_choice.RUNNING.value
-    log.module = m
-    log.home = home
-    log.state = state
-    log.save()
-    event = 'hacku_outgoing' if out else 'hacku_incoming'
-    res = requests.post(
-        url=base_url.format(event=event),
-        json={
-            'value1': m.name,
-            'value2': home,
-            'value3': str(radius)
-        },
-        headers={'Content-Type': 'application/json'}
-    )
-    log.status_code = res.status_code
-    if res.status_code in [i for i in range(200, 209)]:
-        log.status = http_choice.SUCCESS.value
-    else:
-        log.status = http_choice.FAIL.value
-    log.save()
+    for key in settings.IFTTT_KEYS:
+        if key is None:
+            continue
+        log = NotificationLog()
+        http_choice = log.HttpStatusChoice
+        log.status = http_choice.RUNNING.value
+        log.module = m
+        log.home = home
+        log.state = state
+        log.save()
+        event = 'hacku_outgoing' if out else 'hacku_incoming'
+        res = requests.post(
+            url=base_url.format(event=event, IFTTT_KEY=key),
+            json={
+                'value1': m.name,
+                'value2': home,
+                'value3': str(radius)
+            },
+            headers={'Content-Type': 'application/json'}
+        )
+        log.status_code = res.status_code
+        if res.status_code in [i for i in range(200, 209)]:
+            log.status = http_choice.SUCCESS.value
+        else:
+            log.status = http_choice.FAIL.value
+        log.save()
